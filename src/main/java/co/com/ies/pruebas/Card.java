def48a0254ce1cd61c616dd135ba8c82a3ae876b
romariosalas <romariosalas13@gmail.com>
2021-10-03 23:33:59 -0500

    feat(Card): implementar clase card

---
+++ src/main/java/co/com/ies/pruebas/Card.java
@@ -0,0 +1,12 @@
+package co.com.ies.pruebas;
+
+public class Card {
+	
+	public int value;
+	public String stick;
+	public Card(int value, String stick) {
+		super();
+		this.value = value;
+		this.stick = stick;
+	}	
+}
